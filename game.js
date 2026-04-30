ee220d6c6e941c85998afe16d7b0157bc1d9aae6
emmanagle02-glitch <emmanagle02@gmail.com>
2026-04-30 18:38:01 -0400

    Update game.js

--- game.js
+++ game.js
@@ -2,33 +2,18 @@ const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 300 },
-      debug: false
-    }
-  },
+  backgroundColor: '#87CEEB',
   scene: {
-    preload: preload,
-    create: create,
-    update: update
+    create: create
   }
 };
 
 const game = new Phaser.Game(config);
 
-let player;
-let stars;
-let bombs;
-let platforms;
-let cursors;
-let score = 0;
-let scoreText;
-let gameOver = false;
-
-function preload() {
-  this.load.image('sky', 'assets/sky.png');
-  this.load.image('ground', 'assets/platform.png');
-  this
+function create() {
+  this.add.text(220, 280, 'Phaser is working!', {
+    fontSize: '32px',
+    fill: '#000'
+  });
+}
  
