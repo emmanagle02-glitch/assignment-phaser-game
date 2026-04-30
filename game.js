1a0fac4868b0238cad5c9c9cd808592a0ea8a29e
emmanagle02-glitch <emmanagle02@gmail.com>
2026-04-30 18:43:59 -0400

    Update game.js

--- game.js
+++ game.js
@@ -3,17 +3,10 @@ const config = {
   width: 800,
   height: 600,
   backgroundColor: '#87CEEB',
+  parent: 'game',   // 👈 ADD THIS
   scene: {
     create: create
   }
 };
 
-const game = new Phaser.Game(config);
-
-function create() {
-  this.add.text(220, 280, 'Phaser is working!', {
-    fontSize: '32px',
-    fill: '#000'
-  });
-}
  
