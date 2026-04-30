7fda86444e2523b6c6dc1cb0cfb08015d1ae8773
emmanagle02-glitch <emmanagle02@gmail.com>
2026-04-30 17:47:48 -0400

    Add files via upload

---
+++ game.js
@@ -0,0 +1,19 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+const game = new Phaser.Game(config);
+
+function preload () {
+    this.load.image('sky', 'assets/sky.png');
+}
+
+function create () {
+    this.add.image(400, 300, 'sky');
+}
